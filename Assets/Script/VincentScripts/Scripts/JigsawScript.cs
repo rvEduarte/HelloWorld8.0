e00699e1df9412f103a8d119bcfa09d54fde75eb
DESKTOP-MVDTUTR\Rv <rveduarte@gmail.com>
2024-06-28 01:06:07 -0700

    06/28/2024 Updates: WORKING ON ANIMATION OF JIGSAW

--- Assets/Script/VincentScripts/Scripts/JigsawScript.cs
+++ Assets/Script/VincentScripts/Scripts/JigsawScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JigsawScript : MonoBehaviour
 {
@@ -16,22 +17,29 @@ public class JigsawScript : MonoBehaviour
 
     public RunningTimer timer;
 
+    public Button button;
+
     // Use this for initialization
     private void Start()
     {
+        jigsawPanel.GetComponent<GameObject>().transform.localScale = Vector2.zero;
         jigsawPanel.SetActive(false);
         pickUpText.SetActive(false);
         Greenportal.SetActive(false);
+
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(ClickTask);
     }
 
     // Update is called once per frame
     private void Update()
     {
+
         if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
         {
             PickUp();
             timer.isPicked = true;
-            jigsawPanel.SetActive(true);
+            jigsawPanel.GetComponent<GameObject>().transform.LeanScale(Vector2.one, 0.8f);
 
         }
             
@@ -60,5 +68,10 @@ public class JigsawScript : MonoBehaviour
     {
         Greenportal.SetActive(true);
         Destroy(gameObject);
+    }
+
+    public void ClickTask()
+    {
+        jigsawPanel.GetComponent<GameObject>().transform.LeanScale(Vector2.zero, 1f).setEaseInBack();
     }
 }
